e66793775b0f2456aa6a701ddd869b84ef2f87f7
kebowdog <kevin.larry.bauer@gmail.com>
2021-07-22 13:54:58 -0600

    cleaned up unused code in services.js

--- src/services/services.js
+++ src/services/services.js
@@ -25,23 +25,8 @@ export const makeHeader = (reqBody, reqMethod) => {
 }
 
 export const handleErr = (err) => {
-    if(typeof(err) === 'string') window.location.href = window.location.origin + '/error?statusText=' + err.statusText
-
-    if(err.status === 401) {
-        alert("Your session has timed out, please login again to continue")
-        return
-    }
-    if (err.status > 299) {
-        const path = err.url.split('/')
-        let pathTxt = ''
-        for(let i = path.length - 1; i > 0; i--) {
-            if(path[i] === 'api') i = -1
-            else if (i > 0) pathTxt = path[i] + '/' + pathTxt
-        }
-        console.error(err)
-        localStorage['error'] = JSON.stringify(err)
-        window.location.href = window.location.origin + '/error?status=' + err.status + '&url=' + pathTxt + '&statusText=' + err.statusText
-    }
+    // We could expand this to do something fancier in the future, but for now just log the error
+    console.error(err)
     return
 }
 
@@ -57,15 +42,3 @@ export const APICall = (endpoint, method, body) => {
 
 
 export const getHours = () => APICall('rest_hours.json')
-
-export const makeFail = () => {
-    const p = new Promise( function(resolve, reject) {
-        reject({
-            status: 456,
-            url: 'https://api.byu.edu/lifesciences/api/some/endpoint',
-            statusText: "I failed :/"
-        })
-    })
-
-    return p
-}
