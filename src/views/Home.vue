59b5ff4f46a6bc9a05e9fdca16474a55d8165b9d
kebowdog <kevin.larry.bauer@gmail.com>
2021-07-22 10:20:01 -0600

    added more comments, fixed am end time bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,15 +24,41 @@ export default {
     checkIfAvailable(dayToCheck, timeToCheck, availableTime) {
       let dt = new Date(dayToCheck + " " + timeToCheck);
       let dayOfWeek = this.daysOfWeek[dt.getDay() - 1];
+
+      let startDate = new Date(dayToCheck + " " + availableTime.startTime);
+      let endDate = new Date(dayToCheck + " " + availableTime.endTime);
+
       if (availableTime.day == dayOfWeek) {
-        let startDate = new Date(dayToCheck + " " + availableTime.startTime);
-        let endDate = new Date(dayToCheck + " " + availableTime.endTime);
-        
-        return dt >= startDate && dt <= endDate;
+        // I'm assuming for the endDate we want a <
+        return dt >= startDate && dt < endDate;
       }
       return false;
     },
     parseRestaurants(restArray) {
+      // parse the JSON and enrich the data so it's more usable for time comparisons
+      // the end format will look like this:
+      // {
+      //   name : "my restaurant",
+      //   days : [
+      //     {
+      //       day: "Mon",
+      //       startTime: "11:30 am",
+      //       endTime: "9:00 pm"
+      //     },
+      //     // if the end time is in the AM (i.e. 11am - 1am) split it in two, like this:
+      //     {
+      //       day: "Tue",
+      //       startTime: "11:30 am",
+      //       endTime: "11:59 pm"
+      //     }
+      //     {
+      //       day: "Wed",
+      //       startTime: "12:00 am"
+      //       endTime: "1:00 am"
+      //     }
+      //   ]
+      // }
+
       for (let r of restArray) {
         r.days = this.parseTimes(r.times);
         delete r.times;
@@ -64,23 +90,59 @@ export default {
           } else days.push(words[i]);
           i++;
         }
-        let time = words[i]
-        if (time.indexOf(':') == -1)
-            time += ":00"
-        let ampm = words[i + 1]
+        let time = words[i];
+        if (time.indexOf(":") == -1) time += ":00";
+        let ampm = words[i + 1];
         let startTime = time + " " + ampm;
         i += 3; // add three so we skip the time, the am/pm, and the '-'
-        time = words[i]
-        if (time.indexOf(':') == -1)
-            time += ":00"
-        ampm = words[i + 1]
+        time = words[i];
+        if (time.indexOf(":") == -1) time += ":00";
+        ampm = words[i + 1];
         let endTime = time + " " + ampm;
-        for (let i = 0; i < days.length; i++) {
-          days[i] = {
-            day: days[i],
-            startTime: startTime,
-            endTime: endTime,
-          };
+
+        // since the time picker only allows picking at the minute level
+        // we can consider 11:59 pm the latest possible time to check
+        // if the end time is later than 12 am we split it in two below
+        if (endTime == "12:00 am")
+          endTime = "11:59 pm";
+
+        // check if end time is before start time, which would mean
+        // we've wrapped around to the next day
+        let startDate = new Date(this.selectedDate + ' ' + startTime)
+        let endDate = new Date(this.selectedDate + ' ' + endTime)
+        
+        if (endDate > startDate) {
+          for (let i = 0; i < days.length; i++) {
+            days[i] = {
+              day: days[i],
+              startTime: startTime,
+              endTime: endTime,
+            };
+          }
+        } else {
+          // if the end time is in the am and it's less than the start time split it in two
+          let newEndTime = '11:59 pm'
+          let newStartTime = '12:00 am'
+          let newDays = []
+          for (let i = 0; i < days.length; i++) {
+            newDays.push({
+              day: days[i],
+              startTime: startTime,
+              endTime: newEndTime,
+            })
+            
+            let dayIndex = this.daysOfWeek.indexOf(days[i]) + 1;
+            // wrap around for the end of the week
+            if (dayIndex == this.daysOfWeek.length)
+              dayIndex = 0;
+            newDays.push({
+              day: this.daysOfWeek[dayIndex],
+              startTime: newStartTime,
+              endTime: endTime,
+            })
+          }
+          days = newDays
+          console.log(newDays)
         }
         results = results.concat(days);
       }
